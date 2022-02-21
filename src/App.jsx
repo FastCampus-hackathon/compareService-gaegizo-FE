b4b8a97ee4f8ce4924a05b6c2d84bd6410059cd7
iamidlek <iamidlek@naver.com>
2022-02-22 08:54:10 +0900

    feat: add LeftGNB Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import {ThemeProvider} from 'styled-components';
 import {themeDefault} from './theme/default';
 import {GlobalStyles} from './theme/globalStyle';
+import LeftGNB from './LeftNav/LeftGNB';
+import {useState} from 'react';
 
 const App = () => {
+  const [show, setShow] = useState(false);
   return (
     <ThemeProvider theme={themeDefault}>
       <GlobalStyles {...themeDefault} />
+      <LeftGNB show={show} setShow={setShow} />
+      <div>next</div>
     </ThemeProvider>
   );
 };
