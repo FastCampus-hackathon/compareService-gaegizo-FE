5d03679c48e872075a77b1ae7b0d8c23b50907a1
bxxmi <bmi6638@gmail.com>
2022-02-22 01:56:09 +0900

    feat: add CardList component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import {ThemeProvider} from 'styled-components';
 import {themeDefault} from './theme/default';
 import {GlobalStyles} from './theme/globalStyle';
+import CardList from './compareCard/CardList';
 
 const App = () => {
   return (
     <ThemeProvider theme={themeDefault}>
       <GlobalStyles {...themeDefault} />
+      <CardList />
     </ThemeProvider>
   );
 };
