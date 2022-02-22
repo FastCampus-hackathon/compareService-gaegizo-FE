b47013e6c842c986fbdbc9d8a2b249d4dcd42eb0
bxxmi <bmi6638@gmail.com>
2022-02-22 15:13:28 +0900

    style: add image

--- src/scrap/ScrapFilter.jsx
+++ src/scrap/ScrapFilter.jsx
@@ -11,10 +11,14 @@ const ScrapFilter = () => {
         <Filter>폴더 추가</Filter>
       </LeftFilter>
       <RightFilter>
-        <Filter first>전체(3)</Filter>
-        <Filter>전체 선택</Filter>
-        <Filter>폴더 이동</Filter>
-        <Filter>폴더 추가</Filter>
+        <Filter>입사지원만</Filter>
+
+        <Filter>스크랩일순</Filter>
+        <Filter>20</Filter>
+        <Filter>
+          키워드 입력
+          <Image src="./scrap/readingGlasses.png" />
+        </Filter>
       </RightFilter>
     </FilterContainer>
   );
@@ -51,6 +55,13 @@ const Filter = styled.li`
   }
 
   ${(props) => props.first && 'width: 153px;'}
+`;
+
+const Image = styled.img`
+  border: 1px solid red;
+  width: 16px;
+  height: 16px;
+  margin-left: 41px;
 `;
 
 export default ScrapFilter;
